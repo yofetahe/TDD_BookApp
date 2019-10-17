285cf086b1ea520754e3a1395026fe21d7271a55
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-10-17 16:15:55 -0700

    first commit

---
+++ src/main/java/com/example/demo/TDD_BookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDD_BookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDD_BookAppApplication.class, args);
+	}
+
+}
